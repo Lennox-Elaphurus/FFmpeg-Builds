68f889f4c22ff21e758b7ba231ef1e158be6803f
Elaphurus <43885619+Lennox-Elaphurus@users.noreply.github.com>
2021-11-18 13:26:20 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -52,7 +52,7 @@ mkdir ffbuild
 
 FFMPEG_REPO="${FFMPEG_REPO:-https://github.com/Lennox-Elaphurus/FFmpeg.git}"
 FFMPEG_REPO="${FFMPEG_REPO_OVERRIDE:-$FFMPEG_REPO}"
-GIT_BRANCH="${GIT_BRANCH:-master}"
+GIT_BRANCH="${GIT_BRANCH:-main}"
 GIT_BRANCH="${GIT_BRANCH_OVERRIDE:-$GIT_BRANCH}"
 
 BUILD_SCRIPT="$(mktemp)"
